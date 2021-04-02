9376397217dc913ead2eb4507444048bee72bb71
ibelousov <ibelousov@dengisrazy.ru>
2021-04-02 13:34:18 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import {connect} from 'react-redux'
 import Weather from "../src/components/weather_widget/Index";
-import {allCountries, getAll} from "../src/store/weather/actions";
 
 const mapDispatchToProps = {
 }
